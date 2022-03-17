42ab3381cfa9b2037eb97f61ff9a2b3857d7ca0e
TakashimaAyaka <takashimaayaka@Ayaka-macbook-air.local>
2022-03-17 18:56:19 +0900

    commentable.rb内のcreateメソッドの条件分岐をやめて、それぞれのcomments_controller.eb内にcreateメソッドを記載した

--- app/controllers/concerns/commentable.rb
+++ app/controllers/concerns/commentable.rb
@@ -5,28 +5,10 @@ module Commentable
 
   included do
     before_action :set_comment, only: %i[edit update destroy]
-    before_action :set_render, only: %i[create]
   end
 
   def edit
     render '/comments/edit'
-  end
-
-  def create
-    @comment = @commentable.comments.create(comment_params)
-    @comment.user = current_user
-
-    if @comment.save
-      redirect_to @commentable, notice: t('controllers.common.notice_create', name: Comment.model_name.human)
-    elsif @render == 'reports/show'
-      @report = Report.find(params[:report_id])
-      flash.now[:alert] = t('errors.format', attribute: Comment.model_name.human, message: t('errors.messages.empty'))
-      render @render
-    else
-      @book = Book.find(params[:book_id])
-      flash.now[:alert] = t('errors.format', attribute: Comment.model_name.human, message: t('errors.messages.empty'))
-      render @render
-    end
   end
 
   def update
--- app/controllers/reports/comments_controller.rb
+++ app/controllers/reports/comments_controller.rb
@@ -4,6 +4,20 @@ class Reports::CommentsController < ApplicationController
   include Commentable
 
   before_action :set_commentable
+  before_action :set_render, only: %i[create]
+
+  def create
+    @comment = @commentable.comments.create(comment_params)
+    @comment.user = current_user
+
+    if @comment.save
+      redirect_to @commentable, notice: t('controllers.common.notice_create', name: Comment.model_name.human)
+    else
+      @report = Report.find(params[:report_id])
+      flash.now[:alert] = t('errors.format', attribute: Comment.model_name.human, message: t('errors.messages.empty'))
+      render @render
+    end
+  end
 
   private
 
